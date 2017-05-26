2710321a211494bb1937aa07dfab8064b727294e
Yuriy Khomyakov <yuriy.homyakov@gmail.com>
2017-05-26 19:32:14 +0300

    update tests

--- test_app.py
+++ test_app.py
@@ -1,122 +1,184 @@
 # -*- coding: utf-8 -*-
 import unittest
 import mock
-import test_helpers
 
-import os
 import app
-import tempfile
 
-@mock.patch('braintree.ClientToken.generate', staticmethod(lambda: 'test_client_token'))
-@mock.patch('braintree.Transaction.find', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SUCCESSFUL))
+
+class Resp:
+
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
 
-    def test_checkouts_new_route_available(self):
-        res = self.app.get('/checkouts/new')
+    @mock.patch('requests.post')
+    def test_checkouts_usd_ok(self, mock_post):
+        payment = {
+            "Payment": {
+                "ID": 123,
+                "Status": {"ID": 11, "Info": "Captured", "Reasons": []},
+                "CreditCardToken": {"Value": "zxc"}
+            }
+        }
+        mock_post.return_value = Resp(payment, 200)
+        data = {
+            "currency": "USD",
+            "price_USD": "11",
+            "price_EUR": "7",
+            "amount": "2",
+            "card_holder": "CH",
+            "card_number": "333",
+            "card_exp_month": "11",
+            "card_exp_year": "22",
+            "card_cvv": "123",
+        }
+        res = self.app.post('/checkouts', data=data)
+        self.assertEqual(res.status_code, 302)
+        self.assertIn("/checkouts/123", res.location)
+        mock_post.assert_called_with(
+            'https://securetest.smart2pay.com/v1/payments',
+            auth=mock.ANY,
+            json={'Payment': {
+                'MerchantTransactionID': mock.ANY,
+                'Amount': 2200,
+                'Currency': 'USD',
+                'ReturnURL': 'https://smart2pay-demo-db2.herokuapp.com/redirect',
+                'Customer': {'Email': 'youremail@email.com'},
+                'Card': {'HolderName': 'CH', 'Number': '333', 'ExpirationMonth': '11',
+                         'ExpirationYear': '22', 'SecurityCode': '123'},
+                'Capture': True,
+                'GenerateCreditCardToken': True}})
+
+    @mock.patch('requests.post')
+    def test_checkouts_eur_ok(self, mock_post):
+        payment = {
+            "Payment": {
+                "ID": 123,
+                "Status": {"ID": 11, "Info": "Captured", "Reasons": []},
+                "CreditCardToken": {"Value": "zxc"}
+            }
+        }
+        mock_post.return_value = Resp(payment, 200)
+        data = {
+            "currency": "EUR",
+            "price_USD": "11",
+            "price_EUR": "7",
+            "amount": "2",
+            "card_holder": "CH",
+            "card_number": "333",
+            "card_exp_month": "11",
+            "card_exp_year": "22",
+            "card_cvv": "123",
+        }
+        res = self.app.post('/checkouts', data=data)
+        self.assertEqual(res.status_code, 302)
+        self.assertIn("/checkouts/123", res.location)
+        mock_post.assert_called_with(
+            'https://securetest.smart2pay.com/v1/payments',
+            auth=mock.ANY,
+            json={'Payment': {
+                'MerchantTransactionID': mock.ANY,
+                'Amount': 1400,
+                'Currency': 'EUR',
+                'ReturnURL': 'https://smart2pay-demo-db2.herokuapp.com/redirect',
+                'Customer': {'Email': 'youremail@email.com'},
+                'Card': {'HolderName': 'CH', 'Number': '333', 'ExpirationMonth': '11',
+                         'ExpirationYear': '22', 'SecurityCode': '123'},
+                'Capture': True,
+                'GenerateCreditCardToken': True}})
+
+    @mock.patch('requests.get')
+    def test_show(self, mock_get):
+        payment = {
+            "Payment": {
+                "ID": 123321,
+                "Status": {"ID": 11, "Info": "Captured", "Reasons": []},
+                "CreditCardToken": {"Value": "zxc"}
+            }
+        }
+        with app.app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess["payment"] = payment["Payment"]
+            res = c.get('/checkouts/123')
         self.assertEqual(res.status_code, 200)
+        self.assertIn(b"zxc", res.data)
+        self.assertIn(b"123321", res.data)
 
-    def test_root_redirect_to_checkout(self):
-        res = self.app.get('/')
+    @mock.patch('requests.post')
+    def test_checkout_more(self, mock_post):
+        payment = {
+            "Payment": {
+                "ID": 12344,
+                "Status": {"ID": 11, "Info": "Captured", "Reasons": []},
+                "CreditCardToken": {"Value": "zxc"}
+            }
+        }
+        mock_post.return_value = Resp(payment, 200)
+        with app.app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess["payment_method_token"] = {"Value": "xyz222"}
+            res = c.post('/checkouts/one_more', data={"price": "123"})
         self.assertEqual(res.status_code, 302)
-        self.assertIn('/checkouts/new', res.location)
+        self.assertIn("/checkouts/12344", res.location)
+        mock_post.assert_called_with(
+            'https://securetest.smart2pay.com/v1/payments',
+            auth=mock.ANY,
+            json={
+                'Payment': {
+                    'MerchantTransactionID': mock.ANY,
+                    'Amount': 12300,
+                    'Currency': 'USD',
+                    'ReturnURL': 'https://smart2pay-demo-db2.herokuapp.com/redirect',
+                    'Customer': {
+                        'Email': 'youremail@email.com'},
+                    'CreditCardToken': {
+                        'Value': 'xyz222'},
+                    'Capture': True,
+                    'Retry': True,
+                    'GenerateCreditCardToken': True}})
 
-    def test_checkout_contains_client_token(self):
-        res = self.app.get('/checkouts/new')
-        self.assertIn(b'var client_token = \'test_client_token\';', res.data)
-
-    def test_checkout_contains_checkout_form(self):
-        res = self.app.get('/checkouts/new')
-        self.assertIn(b'<form id="payment-form"', res.data)
-
-    def test_checkout_contains_dropin_div(self):
-        res = self.app.get('/checkouts/new')
-        self.assertIn(b'<div id="bt-dropin"', res.data)
-
-    def test_checkout_includes_amount_input(self):
-        res = self.app.get('/checkouts/new')
-        self.assertIn(b'<label for="amount"', res.data)
-        self.assertIn(b'<input id="amount" name="amount" type="tel"', res.data)
-
-    def test_checkouts_show_route_available(self):
-        res = self.app.get('/checkouts/1')
+    @mock.patch('requests.post')
+    @mock.patch('requests.get')
+    def test_refund(self, mock_get, mock_post):
+        payment = {
+            "Payment": {
+                "ID": 321,
+                "Amount": "1200"
+            }
+        }
+        mock_get.return_value = Resp(payment, 200)
+        mock_post.return_value = Resp({"OK": "OK"}, 200)
+        res = self.app.post('/refund', data={'payment_id': 321, "tx_id": "abc"})
         self.assertEqual(res.status_code, 200)
+        self.assertIn(b"OK", res.data)
+        mock_post.assert_called_with(
+            'https://securetest.smart2pay.com/v1/payments/321/refunds',
+            auth=mock.ANY,
+            json={'Refund': {'MerchantTransactionID': 'abc', 'Amount': '1200'}})
 
-    def test_checkouts_show_displays_info(self):
-        res = self.app.get('/checkouts/1')
-        self.assertIn(b'my_id', res.data)
-        self.assertIn(b'10.00', res.data)
-        self.assertIn(b'MasterCard', res.data)
-        self.assertIn(b'ijkl', res.data)
-        self.assertIn(b'Billson', res.data)
-        self.assertIn(b'Billy Bobby Pins', res.data)
-        self.assertIn(b'submitted_for_settlement', res.data)
 
-    def test_checkouts_show_displays_success_message_when_transaction_succeeded(self):
-        res = self.app.get('/checkouts/1')
-        self.assertIn(b'Sweet Success!', res.data)
+    @mock.patch('requests.post')
+    def test_refund_partial(self, mock_post):
+        mock_post.return_value = Resp({"OK": "OK"}, 200)
+        res = self.app.post('/refund/partial',
+                            data={'payment_id': 321, "tx_id": "abc", "amount": 45})
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(b"OK", res.data)
+        mock_post.assert_called_with(
+            'https://securetest.smart2pay.com/v1/payments/321/refunds',
+            auth=mock.ANY,
+            json={'Refund': {'MerchantTransactionID': 'abc', 'Amount': 4500}})
 
-    def test_checkouts_show_displays_failure_message_when_transaction_failed(self):
-        with mock.patch('braintree.Transaction.find', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_FAILURE)):
-            res = self.app.get('/checkouts/1')
-            self.assertIn(b'Transaction Failed', res.data)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_SUCCESSFUL))
-    def test_checkouts_create_redirects_to_checkouts_show(self):
-        res = self.app.post('/checkouts', data={
-            'payment_method_nonce': 'some_nonce',
-            'amount': '12.34',
-        })
-        self.assertEqual(res.status_code, 302)
-        self.assertIn('/checkouts/my_id', res.location)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_SUCCESSFUL))
-    def test_hides_customer_details_if_none(self):
-        with mock.patch('braintree.Transaction.find', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_NO_CUSTOMER)):
-            res = self.app.post('/checkouts', follow_redirects=True, data={
-                'payment_method_nonce': 'some_nonce',
-                'amount': '12.34',
-            })
-
-            self.assertNotIn(b'Customer Details', res.data)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_UNSUCCESSFUL))
-    def test_checkouts_create_redirects_to_checkouts_new_when_transaction_unsuccessful(self):
-        res = self.app.post('/checkouts', data={
-            'payment_method_nonce': 'some_invalid_nonce',
-            'amount': '12.34',
-        })
-        self.assertEqual(res.status_code, 302)
-        self.assertIn('/checkouts/new', res.location)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_UNSUCCESSFUL))
-    def test_checkouts_create_displays_errors_when_transaction_unsuccessful(self):
-        res = self.app.post('/checkouts', follow_redirects=True, data={
-            'payment_method_nonce': 'some_invalid_nonce',
-            'amount': '12.34',
-        })
-        self.assertIn(b'Error: 12345: Transaction was unsuccessful', res.data)
-        self.assertIn(b'Error: 67890: Transaction was really unsuccessful', res.data)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_UNSUCCESSFUL_PROCESSOR))
-    def test_checkouts_create_redirects_to_checkouts_new_when_processor_errors_present(self):
-        res = self.app.post('/checkouts', data={
-            'payment_method_nonce': 'some_invalid_nonce',
-            'amount': '12.34',
-        })
-        self.assertIn('/checkouts/my_id', res.location)
-
-    @mock.patch('braintree.Transaction.sale', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_SALE_UNSUCCESSFUL_PROCESSOR))
-    def test_checkouts_create_displays_errors_when_processor_errors_present(self):
-        with mock.patch('braintree.Transaction.find', staticmethod(lambda x: test_helpers.MockObjects.TRANSACTION_FAILURE)):
-            res = self.app.post('/checkouts', follow_redirects=True, data={
-                'payment_method_nonce': 'some_invalid_nonce',
-                'amount': '2000',
-            })
-            self.assertIn(b'Your test transaction has a status of processor_declined.', res.data)
 
 if __name__ == '__main__':
     unittest.main()
